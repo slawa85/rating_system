bdd2fb12ac9ca88b202dcf8bbe3ca60a12984fe7
Veaceslav Solotchi <slava@vetstor.com>
2026-03-26 19:30:52 +0100

    feat: register Helmet middleware in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import helmet from 'helmet';
+import { AppModule } from './app.module.js';
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter.js';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor.js';
+import { ClsService } from 'nestjs-cls';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger));
+  app.use(helmet());
+
+  const cls = app.get(ClsService);
+  app.useGlobalFilters(new GlobalExceptionFilter(cls));
+  app.useGlobalInterceptors(new LoggingInterceptor(cls));
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
 }
+
 bootstrap();
